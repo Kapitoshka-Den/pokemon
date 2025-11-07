9b93a301ddba54aa3bdf676cbf603f4b1b6652c1
Denis Turkov <DTurkov@mons.ru>
2025-11-08 00:17:14 +0300

    add base

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
+  base: '/pokemon',
   preview: {
     port: 4001,
     host: true,
